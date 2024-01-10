acc070d0e95a05602912472e8827a66d6d49db0c
FusionR1ot <156235870+FusionR1ot@users.noreply.github.com>
2024-01-10 13:07:16 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . d d d d d . . . . . 
+    . d d d d d d d d d . . . 
+    . d d d d d d d d d d . . 
+    d d d d d 1 1 1 1 d d f . 
+    1 d 1 d 1 d d d d d d f . 
+    d 1 d 1 d d d d f f f f . 
+    d d d f f f f e e f f . . 
+    f f e e f b f e e f . . . 
+    . f e 4 e 1 f 4 4 f . . . 
+    . f f f e 4 4 4 4 f . . . 
+    . 2 2 9 9 9 2 2 2 f . . . 
+    . 2 9 4 4 9 c c c f . . . 
+    . . 9 4 4 9 c c c f . . . 
+    . . f 9 9 f d d d f . . . 
+    . . . f f f f f f . . . . 
+    . . . . f f f . . . . . . 
+    `, SpriteKind.Player)
